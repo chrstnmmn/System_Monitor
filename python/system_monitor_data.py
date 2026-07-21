40b94fadbc8efab725c11a74f0d5cce0eb09056a
chrstnmmnl <christianemmanuelcollo23@gmail.com>
2026-07-21 16:40:11 +0800

    cleaning the output so that it will print on the same line

--- python/system_monitor_data.py
+++ python/system_monitor_data.py
@@ -2,7 +2,7 @@ import psutil
 
 while True:
     cpu_usage = psutil.cpu_percent(interval=1)
-    print(f"CPU Utilization: {cpu_usage}%")
+    print(f"\rCPU Utilization: {cpu_usage}%       ", end="", flush=True)
     
     
     
